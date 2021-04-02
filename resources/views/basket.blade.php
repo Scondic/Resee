d996a105d107bffb2ba152b6031d1dd6fe9dfbec
Scondic <scondic@icloud.com>
2021-04-03 00:31:31 +0300

    Добавление Tailwind CSS. NPM Install

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -1,4 +1,19 @@
+@php
+    $curl = curl_init("https://easydonate.ru/api/v1/shop/4223c208eeec0d60b28e04f579967328");
+    $headers = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'User-Agent: Scondic'
+    );
 
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $data = curl_exec($curl);
+
+    dd($data);
+@endphp
 
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
@@ -6,8 +21,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Resee — Корзина</title>
+
+        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
     </head>
     <body>
-
+        <h1>Корзина покупок</h1>
     </body>
 </html>
